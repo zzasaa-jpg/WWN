573f2a22b4eff220c685f010a79ab0367876c028
Mahendra890() <130951660+zzasaa-jpg@users.noreply.github.com>
2023-12-28 21:54:25 +0530

    Update News.js
    ui changed

--- src/News.js
+++ src/News.js
@@ -8,13 +8,12 @@ function News(props) {
     <>
     
     <div className='section'>
-      <div className='img'>
         {
           props.article.urlToImage !== null ?
-            <img src={props.article.image} alt='logo'/> :
+            <img className='img' src={props.article.image} alt='logo'/> :
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmHl1hOB60MKFw_j9QUVQKAguWs1C2p1YXGQ&usqp=CAU" alt='logo'/>
-
         }
+      <div className='section-h2-p'>
         <h2 id='title'>{props.article.title}</h2>
         <p id='des'>{props.article.description?.substring(0, 100).concat('...')} <a id='link' href={props.article.url} target='_blank' rel='noopener noreferrer'>Read more</a></p>
         <p id='publish-date'>{props.article.publishedAt}</p>
